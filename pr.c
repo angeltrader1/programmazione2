587f7f84772b9e4945c7d3474586903161e7efeb
angeltrader1 <angeltrader1>
2025-03-14 14:31:35 +0100

    nuova

---
+++ pr.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int x = 9;
+    printf("Il valore di x %d\n ",x);
+    int y = 3;
+    printf("Il valore di y %d\n ",y);
+    printf("\nCiao");
+    printf("\nSiamo andati in montagna");
+    return 0;
+}
